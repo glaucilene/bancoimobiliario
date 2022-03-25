f454d16136d78b0b20a6e1833c37e0e0f5a7096a
Glaucilene <glauci@gmail.com>
2022-03-24 20:39:26 -0400

    criada classe e alterada a função para sortear jogadores

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import random
 
-def sortear_jogadores(lista):
-    lista = random.sample(lista, 4)
-    return len(lista)
+class Simulador():
+
+    def __init__(self, numero_jogadores):
+        self.numero_jogadores = numero_jogadores
+
+    def sortear_jogadores(self):
+        lista = [i+1 for i in range(self.numero_jogadores)]
+        lista = random.sample(lista, self.numero_jogadores)
+        return lista
 
 if __name__ == '__main__':
-    sortear_jogadores([1,2,3,4])
+    simulador = Simulador(numero_jogadores=4)
+    print(Simulador.sortear_jogadores([1,2,3,4], tamanho=4))
--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
 import unittest
-from main import sortear_jogadores
+from main import Simulador
 
 class TestBanco(unittest.TestCase):
     def test_checarjogadores(self):
-        self.assertEqual(sortear_jogadores([1,2,3,4]), 4)
+        self.assertEqual(Simulador(numero_jogadores=4).sortear_jogadores(), [1,2,3,4])
 
 if __name__ == '__main__':
     unittest.main()
