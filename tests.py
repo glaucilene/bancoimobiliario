78c9a42a4a0ae6c15f37115a3b6bb04f8111c86f
Glaucilene <glauci@gmail.com>
2022-04-07 20:31:15 -0400

    criadas classes

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 import unittest
-from main import Simulador
+from main import Simulator
 
-class TestBanco(unittest.TestCase):
-    def test_checarjogadores(self):
-        self.assertEqual(Simulador(numero_jogadores=4).sortear_jogadores(), [1,2,3,4])
+class TestBank(unittest.TestCase):
+    def test_checkplayers(self):
+        self.assertEqual(Simulator(number_players=4).sort_players(), [1,2,3,4])
+    def test_turns(self):
+        self.assertEqual(Simulator().turns(), None)
 
 if __name__ == '__main__':
     unittest.main()
